6b8b6f98937c161ef77a9f3e0bc5a37e94b3ed8e
Armando Ruocco <armando.ruocco@enterprisedb.com>
2025-07-15 17:02:21 +0200

    chore: domain leftovers
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- core/internal/cnpgi/metadata.go
+++ core/internal/cnpgi/metadata.go
@@ -4,7 +4,7 @@ import "github.com/cloudnative-pg/cnpg-i/pkg/identity"
 
 // data is the metadata of this plugin.
 var data = identity.GetPluginMetadataResponse{ //nolint:gochecknoglobals
-	Name:          "klio.enterprisedb.com",
+	Name:          "klio.cnpg.io",
 	Version:       "0.0.1",
 	DisplayName:   "Klio",
 	ProjectUrl:    "",
