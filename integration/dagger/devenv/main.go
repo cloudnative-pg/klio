936797dbd251ea06cf0f2f38c04714bae07baaf0
cnp-autobot <85171364+cnp-autobot@users.noreply.github.com>
2025-08-22 10:38:09 +0200

    chore(deps): update alpine/k8s docker tag to v1.33.4 (EnterpriseDB/klio#196)
    | datasource | package    | from   | to     |
    | ---------- | ---------- | ------ | ------ |
    | docker     | alpine/k8s | 1.33.3 | 1.33.4 |
    
    Signed-off-by:
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- integration/dagger/devenv/main.go
+++ integration/dagger/devenv/main.go
@@ -51,7 +51,7 @@ func (m *Devenv) Kubernetes(
 // +default="rancher/k3s:v1.33.3-k3s1"
 	k3SImage string,
 // renovate image: datasource=docker depName=alpine/k8s versioning=docker
-// +default="alpine/k8s:1.33.3"
+// +default="alpine/k8s:1.33.4"
 	alpineK8S string,
 ) (*dagger.Container, error) {
 	klioImage := source.
