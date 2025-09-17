73a565642c82471f552216b54662586d5dde6c1e
Armando Ruocco <armando.ruocco@enterprisedb.com>
2025-09-17 16:02:13 +0200

    fix: maintain the old service name or everything breaks
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- operator/api/v1alpha1/server_types.go
+++ operator/api/v1alpha1/server_types.go
@@ -113,5 +113,5 @@ func init() {
 
 // GetServiceName returns the name of the service associated with the Klio server.
 func (s *Server) GetServiceName() string {
-	return s.Name + "-klio"
+	return s.Name
 }
