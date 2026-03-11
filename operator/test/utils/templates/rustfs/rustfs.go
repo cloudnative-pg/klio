aded12bb08ddaf338b3410f29731d3c71a3260b1
cnp-autobot <85171364+cnp-autobot@users.noreply.github.com>
2026-03-11 22:10:12 +0100

    chore(deps): update amazon/aws-cli docker tag to v2.34.7 (EnterpriseDB/klio#1041)
    | datasource | package        | from   | to     |
    | ---------- | -------------- | ------ | ------ |
    | docker     | amazon/aws-cli | 2.34.6 | 2.34.7 |
    
    Signed-off-by:
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- operator/test/utils/templates/rustfs/rustfs.go
+++ operator/test/utils/templates/rustfs/rustfs.go
@@ -28,7 +28,7 @@ const (
 	// AWSCLIImage is the AWS CLI container image used for S3 operations.
 	//nolint:godot
 	// renovate image: datasource=docker depName=amazon/aws-cli versioning=docker
-	AWSCLIImage = "amazon/aws-cli:2.34.6@sha256:9fa5b5ebad9d20dbecbbffedec16010dbe99102565facf3fd919b1888112135e"
+	AWSCLIImage = "amazon/aws-cli:2.34.7@sha256:9ef589924a9d9df06193db6a57ef88c5ccae1d19a71c6ac545bec3915f34f520"
 
 	// RustFSAccessKey is the access key for RustFS.
 	RustFSAccessKey = "rustfsaccesskey1234567890"
