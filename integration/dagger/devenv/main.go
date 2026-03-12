2598a33d430cadfc2bd18aca36129027bb352d2e
cnp-autobot <85171364+cnp-autobot@users.noreply.github.com>
2026-03-12 19:18:30 +0100

    chore(deps): update skopeo:v1.22.0 docker digest to 0cc3bbf (EnterpriseDB/klio#1046)
    Signed-off-by:
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- integration/dagger/devenv/main.go
+++ integration/dagger/devenv/main.go
@@ -45,7 +45,7 @@ func (m *Devenv) Kubernetes(
 	registryImage string,
 // +optional
 // renovate image: datasource=docker depName=skopeo lookupName=quay.io/skopeo/stable versioning=docker
-// +default="quay.io/skopeo/stable:v1.22.0@sha256:2dae5d328c5513438e1dfb534057d3844f345d20f8245743e1334a0b7a1f64ff"
+// +default="quay.io/skopeo/stable:v1.22.0@sha256:0cc3bbfeb53b0a5d70c68b8b10daa74ec2bd5e4e6f2db3ba7d4c678354e7b171"
 	skopeoImage string,
 // renovate image: datasource=docker depName=k3s lookupName=rancher/k3s versioning=docker
 // +default="rancher/k3s:v1.35.2-k3s1@sha256:c3184157c3048112bab0c3e17405991da486cb3413511eba23f7650efd70776b"
