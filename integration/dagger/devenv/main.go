0eb8a0f98caedff83fe5a00db56a06e88ff9335b
cnp-autobot <85171364+cnp-autobot@users.noreply.github.com>
2026-04-17 20:19:33 +0200

    chore(deps): update skopeo docker tag to v1.22.2 (EnterpriseDB/klio#1284)
    | datasource | package               | from    | to      |
    | ---------- | --------------------- | ------- | ------- |
    | docker     | quay.io/skopeo/stable | v1.22.0 | v1.22.2 |
    
    Signed-off-by:
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- integration/dagger/devenv/main.go
+++ integration/dagger/devenv/main.go
@@ -45,7 +45,7 @@ func (m *Devenv) Kubernetes(
 	registryImage string,
 // +optional
 // renovate image: datasource=docker depName=skopeo lookupName=quay.io/skopeo/stable versioning=docker
-// +default="quay.io/skopeo/stable:v1.22.0@sha256:06dd47ee861e143268f0b811cdf1f9d6509b097945de447dbcdfb668ca15364c"
+// +default="quay.io/skopeo/stable:v1.22.2@sha256:4a16d57b37617a04b3d643079a477a2848efe892dffcdf0ce56df4262b65f810"
 	skopeoImage string,
 // renovate image: datasource=docker depName=k3s lookupName=rancher/k3s versioning=docker
 // +default="rancher/k3s:v1.35.3-k3s1@sha256:4607083d3cac07e1ccde7317297271d13ed5f60f35a78f33fcef84858a9f1d69"
