a6858f6acc4033f33826162a289c94c06b27ba42
Gabriele Fedi <gabriele.fedi@enterprisedb.com>
2026-04-28 12:35:41 +0200

    ci: configure renovate postUpgradeTasks (EnterpriseDB/klio#1338)
    Signed-off-by: Gabriele Fedi <gabriele.fedi@enterprisedb.com>
    Signed-off-by: Gabriele Quaresima <gabriele.quaresima@enterprisedb.com>
    Co-authored-by: Gabriele Quaresima <gabriele.quaresima@enterprisedb.com>

---
+++ .github/renovate-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+# renovate: datasource=github-releases depName=go-task/task extractVersion=^v(?<version>.+)$
+TASK_VERSION='3.50.0'
+# renovate: datasource=github-releases depName=dagger/dagger extractVersion=^v(?<version>.+)$
+export DAGGER_VERSION='0.20.6'
+
+# Install Task (https://taskfile.dev)
+sh -c "$(curl --proto "=https" --tlsv1.2 -sSf -L https://taskfile.dev/install.sh)" -- -d -b /usr/local/bin "v${TASK_VERSION}"
+
+# Install Dagger (https://docs.dagger.io)
+curl --proto "=https" --tlsv1.2 -sSf -L https://dl.dagger.io/dagger/install.sh | BIN_DIR=/usr/local/bin sh
+
+# Run Renovate
+renovate
