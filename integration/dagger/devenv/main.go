0145825650d8e7bccede9340608b0191b182a86a
cnp-autobot <85171364+cnp-autobot@users.noreply.github.com>
2026-05-05 00:22:31 +0200

    chore(deps): update registry:3.1 docker digest to 85347ed (EnterpriseDB/klio#1412)
    Signed-off-by:
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- integration/dagger/devenv/main.go
+++ integration/dagger/devenv/main.go
@@ -41,7 +41,7 @@ func (m *Devenv) Kubernetes(
 	source *dagger.Directory,
 // +optional
 // renovate image: datasource=docker depName=registry versioning=docker
-// +default="registry:3.1@sha256:8a7c1aae9db5028cbc1cfe83e5969d270a50e8bc0b72eef3cc5657542ec383c9"
+// +default="registry:3.1@sha256:85347ed2ecde64161c7a4788a4d7d3dcc9d6f86f7be95834022e3c6a423a945a"
 	registryImage string,
 // +optional
 // renovate image: datasource=docker depName=skopeo lookupName=quay.io/skopeo/stable versioning=docker
