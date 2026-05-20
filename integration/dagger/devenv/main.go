49562010bcae691e465b392dd6a4365802cc65f5
cnp-autobot <85171364+cnp-autobot@users.noreply.github.com>
2026-05-21 01:29:36 +0200

    chore(deps): update k3s docker tag to v1.36.1 (EnterpriseDB/klio#1522)
    | datasource | package     | from    | to      |
    | ---------- | ----------- | ------- | ------- |
    | docker     | rancher/k3s | v1.36.0 | v1.36.1 |
    
    Signed-off-by:
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- integration/dagger/devenv/main.go
+++ integration/dagger/devenv/main.go
@@ -48,7 +48,7 @@ func (m *Devenv) Kubernetes(
 // +default="quay.io/skopeo/stable:v1.22.2@sha256:4d6a1f31b90dbce1c019aba61292136ea9903573718464f3448ec439c6dfc66f"
 	skopeoImage string,
 // renovate image: datasource=docker depName=k3s lookupName=rancher/k3s versioning=docker
-// +default="rancher/k3s:v1.36.0-k3s1@sha256:38069dc4a22a056f427f4f37c887303697cc817f7735e57dd142e169af7f398f"
+// +default="rancher/k3s:v1.36.1-k3s1@sha256:08fdebd14db9ab7d5ea821d5bfa95d02341a6ef886842fcc8d9dfd0e9fa9e0cd"
 	k3SImage string,
 // renovate image: datasource=docker depName=alpine/k8s versioning=docker
 // +default="alpine/k8s:1.35.0@sha256:b01ed7ee5807e1abce433fba29447595b6157851054a649c2aafd6c22a3aa16c
