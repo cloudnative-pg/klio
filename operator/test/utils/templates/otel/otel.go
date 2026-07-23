aa6533eaf33d24f32cf71e15a2a5d5dda4c2ff47
cnp-autobot <85171364+cnp-autobot@users.noreply.github.com>
2026-07-23 05:04:21 +0200

    chore(deps): update otel/opentelemetry-collector-contrib docker tag to v0.157.0 (EnterpriseDB/klio#1851)
    | datasource | package                              | from    | to      |
    | ---------- | ------------------------------------ | ------- | ------- |
    | docker     | otel/opentelemetry-collector-contrib | 0.156.0 | 0.157.0 |
    
    Signed-off-by:
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- operator/test/utils/templates/otel/otel.go
+++ operator/test/utils/templates/otel/otel.go
@@ -16,7 +16,7 @@ const (
 	// CollectorImage is the OTEL collector image.
 	//nolint:godot,lll
 	// renovate image: datasource=docker depName=otel/opentelemetry-collector-contrib versioning=docker
-	CollectorImage = "otel/opentelemetry-collector-contrib:0.156.0@sha256:125bdbeb7590cc1952c5b3430ecf14063568980c2c93d5b38676cc0446ed8108"
+	CollectorImage = "otel/opentelemetry-collector-contrib:0.157.0@sha256:f2f01157055a9b2aab9df7118e1f1c9abf345e99b23bc7a2bc791db374a7d0f6"
 	// CollectorPort is the gRPC port for the OTEL collector.
 	CollectorPort = 4317
 	// CollectorMetricsPort is the Prometheus metrics port for the OTEL collector.
