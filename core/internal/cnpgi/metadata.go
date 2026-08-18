bc06ca6483ca1cd4c5b50f860190824753bf54ae
John Long <john.long@enterprisedb.com>
2026-08-18 14:28:51 -0400

    fix(core): add release-please marker to core plugin version
    Closes #130
    
    Signed-off-by: John Long <john.long@enterprisedb.com>

--- core/internal/cnpgi/metadata.go
+++ core/internal/cnpgi/metadata.go
@@ -24,7 +24,7 @@ import "github.com/cloudnative-pg/cnpg-i/pkg/identity"
 // data is the metadata of this plugin.
 var data = identity.GetPluginMetadataResponse{ //nolint:gochecknoglobals
 	Name:          "klio.cnpg.io",
-	Version:       "0.0.1",
+	Version:       "0.0.20", // x-release-please-version
 	DisplayName:   "Klio",
 	ProjectUrl:    "",
 	RepositoryUrl: "",
